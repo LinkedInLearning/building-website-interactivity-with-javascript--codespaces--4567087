33c8d8258fd8d2aa0354dd3330a757f27b7e6017
chellman <joe@shooflydesign.org>
2024-07-10 22:36:36 +0000

    Final 04/04 state

--- Ch04/05-begin/tables.js
+++ Ch04/05-begin/tables.js
@@ -1,87 +1,85 @@
-"use strict";
-
+'use strict';
 
 let tableData = [];
 
-const collectTableData = function(myTable) {
-    const body = myTable.tBodies[0];
+const collectTableData = function (myTable) {
+  const body = myTable.tBodies[0];
 
-    let tableData = [];
+  let tableData = [];
 
-    for (const row of body.rows) {
-        const cells = row.cells;
-        let dataRow = [cells[0].innerText, cells[1].innerText];
-        dataRow[2] = parseInt(cells[2].innerText, 10);
-        dataRow[3] = parseInt(cells[3].innerText, 10);
+  for (const row of body.rows) {
+    const cells = row.cells;
 
-        tableData.push(dataRow);
-    }
+    let dataRow = [cells[0].innerText, cells[1].innerText];
+    dataRow[2] = parseInt(cells[2].innerText, 10);
+    dataRow[3] = parseInt(cells[3].innerText, 10);
 
-    return tableData;
-}
+    tableData.push(dataRow);
+  }
+
+  return tableData;
+};
 
 window.addEventListener('DOMContentLoaded', () => {
-    // collect table data into an array
-    const myTable = document.querySelector('.content table');
+  const myTable = document.querySelector('.content table');
 
-    tableData = collectTableData(myTable);
+  tableData = collectTableData(myTable);
 
-    console.log('data collected', [].concat(tableData));
+  console.log('data collected', [].concat(tableData));
 
-    const tableHeader = document.querySelector('table thead');
+  const tableHeader = document.querySelector('table thead');
 
-    tableHeader.addEventListener('click', evt => {
-       const target = evt.target;
-       const targetId = target.id;
-       const targetElement = document.getElementById(targetId);
+  tableHeader.addEventListener('click', (evt) => {
+    const targetElement = evt.target;
+    const targetId = targetElement.id;
 
-       if (!targetElement.matches('.sortable')) {
-           return;
-       }
+    if (!targetElement.matches('.sortable')) {
+      return;
+    }
 
-       let sortKey;
-       let sortType = 'alpha';
-       let sortDirection;
+    let sortKey;
+    let sortType = 'alpha';
+    let sortDirection;
 
-        switch (targetId) {
-            case 'header-name':
-                sortKey = 0;
-                break;
+    switch (targetId) {
+      case 'header-name':
+        sortKey = 0;
+        break;
+    }
+
+    let currentSortDirection = targetElement.getAttribute('aria-sort');
+    switch (currentSortDirection) {
+      case 'ascending':
+        sortDirection = 'descending';
+        break;
+      case 'descending':
+      default:
+        sortDirection = 'ascending';
+        break;
+    }
+
+    tableData.sort((a, b) => {
+      if (a[sortKey] === b[sortKey]) {
+        return 0;
+      }
+
+      if (sortDirection === 'ascending') {
+        if (a[sortKey] < b[sortKey]) {
+          return -1;
+        } else if (a[sortKey] > b[sortKey]) {
+          return 1;
         }
-
-        let currentSortDirection = targetElement.getAttribute('aria-sort');
-        switch (currentSortDirection) {
-            case 'ascending':
-                sortDirection = 'descending';
-                break;
-            case 'descending':
-            default:
-                sortDirection = 'ascending';
-                break;
+      } else {
+        if (b[sortKey] < a[sortKey]) {
+          return -1;
+        } else if (b[sortKey] > a[sortKey]) {
+          return 1;
         }
-
-        tableData.sort((a, b) => {
-            if (a[sortKey] === b[sortKey]) {
-                return 0;
-            }
-
-            if (sortDirection === 'ascending') {
-                if (a[sortKey] < b[sortKey]) {
-                    return -1;
-                } else if (a[sortKey] > b[sortKey]) {
-                    return 1;
-                }
-            } else {
-                if (b[sortKey] < a[sortKey]) {
-                    return -1;
-                } else if (b[sortKey] > a[sortKey]) {
-                    return 1;
-                }
-            }
-        });
-
-        targetElement.setAttribute('aria-sort', sortDirection);
-
-        console.log('data sorted', tableData);
+      }
     });
+
+    targetElement.setAttribute('aria-sort', sortDirection);
+
+    console.log('data sorted', tableData);
+  });
 });
