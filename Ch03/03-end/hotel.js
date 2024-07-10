329d1bbbbddac88448e09b40e50b0b70dd81ed25
chellman <joe@shooflydesign.org>
2024-07-10 18:13:49 +0000

    Finalized JS for end of 03/03

--- Ch03/03-end/hotel.js
+++ Ch03/03-end/hotel.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const btnGirado = document.querySelector('#btn-chef-g');
-    const dialogGirardo = document.querySelector('#dlg-chef-g');
-    const btnClose = document.querySelector('.btn--dialog-close');
+  const btnGirado = document.querySelector('#btn-chef-g');
+  const dialogGirardo = document.querySelector('#dlg-chef-g');
+  const btnClose = document.querySelector('.btn--dialog-close');
 
-    btnGirado.addEventListener('click', evt => {
-        evt.preventDefault();
+  btnGirado.addEventListener('click', (evt) => {
+    evt.preventDefault();
 
-        dialogGirardo.showModal();
-    });
+    dialogGirardo.showModal();
+  });
 
-    btnClose.addEventListener('click', evt => {
-        evt.preventDefault();
+  btnClose.addEventListener('click', (evt) => {
+    evt.preventDefault();
 
-        dialogGirardo.close();
-    });
+    dialogGirardo.close();
+  });
 });
