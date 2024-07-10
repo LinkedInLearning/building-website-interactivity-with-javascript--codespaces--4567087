68758bd1e4d1af2e43919b0d2db4331ab99c4ae0
chellman <joe@shooflydesign.org>
2024-07-10 20:33:08 +0000

    Tweak to Ch3 challenge JS

--- Ch03/08-end/password.js
+++ Ch03/08-end/password.js
@@ -1,53 +1,56 @@
-"use strict";
+'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const myForm = document.querySelector('#frm-signup');
-    const passwords = document.querySelectorAll('.field--password');
+  const myForm = document.querySelector('#frm-signup');
+  const passwords = document.querySelectorAll('.field--password');
 
-    const messagesRegion = document.querySelector('#messages');
-    const successDialog = messagesRegion.querySelector('.dialog--message--success');
-    const errorDialog = messagesRegion.querySelector('.dialog--message--error');
+  const messagesRegion = document.querySelector('#messages');
+  const successDialog = messagesRegion.querySelector(
+    '.dialog--message--success'
+  );
+  const errorDialog = messagesRegion.querySelector('.dialog--message--error');
 
-    let closeTimer;
+  let closeTimer;
 
-    // Step 1: Validate that the passwords match when the form is submitted.
-    myForm.addEventListener('submit', evt => {
-        // For our purposes, this form doesn't need to go anywhere
-        evt.preventDefault();
+  // Step 1: Validate that the passwords match when the form is submitted.
+  myForm.addEventListener('submit', (evt) => {
+    // For our purposes, this form doesn't need to go anywhere
+    evt.preventDefault();
 
-        if (passwords[0].value !== passwords[1].value) {
-            successDialog.close();
-            errorDialog.textContent = `Your passwords must match.  Please double check and fix them.`;
-            errorDialog.show();
-        } else {
-            errorDialog.close();
-            successDialog.textContent = `Success!`;
-            successDialog.show();
+    if (passwords[0].value !== passwords[1].value) {
+      successDialog.close();
+      errorDialog.textContent =
+        'Your passwords must match.  Please double check and fix them.';
+      errorDialog.show();
+    } else {
+      errorDialog.close();
+      successDialog.textContent = 'Success!';
+      successDialog.show();
 
-            if (successDialog.dataset.closeDelay) {
-                const delay = parseInt(successDialog.dataset.closeDelay, 10);
+      if (successDialog.dataset.closeDelay) {
+        const delay = parseInt(successDialog.dataset.closeDelay, 10);
 
-                closeTimer = window.setTimeout(() => {
-                    successDialog.close();
-                }, delay * 1000)
-            }
-        }
+        closeTimer = window.setTimeout(() => {
+          successDialog.close();
+        }, delay * 1000);
+      }
+    }
+  });
+
+  // Step 2: Wire up toggling password visibility
+  const btnReveal = document.getElementById('reveal-password');
+
+  btnReveal.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    passwords.forEach((pwField) => {
+      if (pwField.type === 'password') {
+        pwField.type = 'text';
+        btnReveal.innerText = btnReveal.dataset.labelHide;
+      } else {
+        pwField.type = 'password';
+        btnReveal.innerText = btnReveal.dataset.labelShow;
+      }
     });
-
-    // Step 2: Wire up toggling password visibility
-    const btnReveal = document.getElementById('reveal-password');
-
-    btnReveal.addEventListener('click', evt => {
-        evt.preventDefault();
-
-        passwords.forEach(pwField => {
-            if (pwField.type === 'password') {
-                pwField.type = 'text';
-                btnReveal.innerText = btnReveal.dataset.labelHide;
-            } else {
-                pwField.type = 'password';
-                btnReveal.innerText = btnReveal.dataset.labelShow;
-            }
-        });
-    });
+  });
 });
