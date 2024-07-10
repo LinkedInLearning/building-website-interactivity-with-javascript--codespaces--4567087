e523fe12648b00a1814448b61bcc8d8e3969c35d
chellman <joe@shooflydesign.org>
2024-07-10 16:31:09 +0000

    Prettier pass on 03/02

--- Ch03/02-begin/hotel.js
+++ Ch03/02-begin/hotel.js
@@ -1,35 +1,35 @@
-"use strict";
+'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const btnGirado = document.querySelector('#btn-chef-g');
+  const btnGirado = document.querySelector('#btn-chef-g');
 
-    btnGirado.addEventListener('click', evt => {
-        evt.preventDefault();
+  btnGirado.addEventListener('click', (evt) => {
+    evt.preventDefault();
 
-        // Regular string
-        // alert("Chef Girado\n\nChef Girado trained in Paris and has a degree in cuisinology from Tufts.");
+    // Regular string
+    // alert("Chef Girado\n\nChef Girado trained in Paris and has a degree in cuisinology from Tufts.");
 
-        // Leaving the info in the page, pulling into an alert means questionable formatting
-        const infoGirado = document.querySelector('#info-chef-g');
-        alert(infoGirado.textContent);
-    });
+    // Leaving the info in the page, pulling into an alert means questionable formatting
+    const infoGirado = document.querySelector('#info-chef-g');
+    alert(infoGirado.textContent);
+  });
 
-    const btnStiles = document.querySelector('#btn-chef-s');
-    const dialogStiles = document.querySelector('.modal-dialog');
-    const dialogStilesBackdrop = document.querySelector('.modal-dialog-backdrop');
-    const btnStilesClose = document.querySelector('#btn-chef-s-close');
+  const btnStiles = document.querySelector('#btn-chef-s');
+  const dialogStiles = document.querySelector('.modal-dialog');
+  const dialogStilesBackdrop = document.querySelector('.modal-dialog-backdrop');
+  const btnStilesClose = document.querySelector('#btn-chef-s-close');
 
-    btnStiles.addEventListener('click', evt => {
-        evt.preventDefault();
+  btnStiles.addEventListener('click', (evt) => {
+    evt.preventDefault();
 
-        dialogStiles.style.display = 'block';
-        dialogStilesBackdrop.style.display = 'block';
-    });
+    dialogStiles.style.display = 'block';
+    dialogStilesBackdrop.style.display = 'block';
+  });
 
-    btnStilesClose.addEventListener('click', evt => {
-        evt.preventDefault();
+  btnStilesClose.addEventListener('click', (evt) => {
+    evt.preventDefault();
 
-        dialogStiles.style.display = 'none';
-        dialogStilesBackdrop.style.display = 'none';
-    });
+    dialogStiles.style.display = 'none';
+    dialogStilesBackdrop.style.display = 'none';
+  });
 });
