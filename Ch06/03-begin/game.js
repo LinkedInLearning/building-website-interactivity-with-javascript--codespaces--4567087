4fa601f178bf97cca3e8c8068e73e0c82c0344b0
Joe Chellman <joe@shooflydesign.org>
2024-07-09 23:39:43 -0700

    Initial commit of a few videos for the game chapter

---
+++ Ch06/03-begin/game.js
@@ -0,0 +1,42 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', () => {
+    const boardRegion = document.querySelector('.board');
+    const slotsRegion = document.querySelector('.slots');
+    const tilesRegion = document.querySelector('.tiles');
+
+    let currentSolution = [];
+
+    function addTileToSolution(tile) {
+        const letter = tile.dataset.letter;
+
+        const targetSlotIndex = currentSolution.length + 1;
+        const targetSlot = document.querySelector(`#slot-${targetSlotIndex}`);
+
+        // No slots left
+        if (!targetSlot) {
+            return;
+        }
+
+        currentSolution.push(letter);
+        console.log('Solution so far:', currentSolution.join(''));
+
+        tile.style.left = targetSlot.offsetLeft + 'px';
+        tile.style.top = targetSlot.offsetTop + 'px';
+        tile.style.transform = 'none';
+    }
+
+    tilesRegion.addEventListener('click', evt => {
+        evt.preventDefault();
+
+        const tile = evt.target;
+
+        if (!tile.matches('.tile')) {
+            return;
+        }
+
+        if (currentSolution.indexOf(tile.dataset.letter) === -1) {
+            addTileToSolution(tile);
+        }
+    });
+})
---
+++ Ch06/03-end/game.js
@@ -0,0 +1,73 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', () => {
+    const boardRegion = document.querySelector('.board');
+    const slotsRegion = document.querySelector('.slots');
+    const tilesRegion = document.querySelector('.tiles');
+
+    let currentSolution = [];
+
+    function addTileToSolution(tile) {
+        const letter = tile.dataset.letter;
+
+        const targetSlotIndex = currentSolution.length + 1;
+        const targetSlot = document.querySelector(`#slot-${targetSlotIndex}`);
+
+        // No slots left
+        if (!targetSlot) {
+            return;
+        }
+
+        currentSolution.push(letter);
+        console.log('Solution so far:', currentSolution.join(''));
+
+        // Store the tile's original location if needed
+        if (!tile.dataset.originalLeft) {
+            const originalStyle = getComputedStyle(tile);
+
+            tile.dataset.originalLeft = originalStyle.getPropertyValue('left');
+            tile.dataset.originalTop = originalStyle.getPropertyValue('top');
+            tile.dataset.originalTransform = originalStyle.getPropertyValue('transform');
+        }
+
+
+        tile.style.left = targetSlot.offsetLeft + 'px';
+        tile.style.top = targetSlot.offsetTop + 'px';
+        tile.style.transform = 'none';
+    }
+
+    function removeLetterFromSolution(tile) {
+        const letter = tile.dataset.letter;
+
+        // remove the letter from the solution, restore the tile
+        const lastLetter = currentSolution.slice(-1).toString();
+
+        if (!lastLetter || letter !== lastLetter) {
+            console.error('Letters must be removed in order:', letter, lastLetter);
+            return;
+        }
+
+        currentSolution = currentSolution.slice(0, -1);
+        console.log('Solution so far:', currentSolution.join(''));
+
+        tile.style.left = tile.dataset.originalLeft;
+        tile.style.top = tile.dataset.originalTop;
+        tile.style.transform = tile.dataset.originalTransform;
+    }
+
+    tilesRegion.addEventListener('click', evt => {
+        evt.preventDefault();
+
+        const tile = evt.target;
+
+        if (!tile.matches('.tile')) {
+            return;
+        }
+
+        if (currentSolution.indexOf(tile.dataset.letter) === -1) {
+            addTileToSolution(tile);
+        } else {
+            removeLetterFromSolution(tile);
+        }
+    });
+})
