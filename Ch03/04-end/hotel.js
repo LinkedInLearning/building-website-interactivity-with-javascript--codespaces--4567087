da922d9c7c29abd4441f0c6996d99b9ed9eda6c2
chellman <joe@shooflydesign.org>
2024-07-10 18:59:21 +0000

    Updated end for 03/04

--- Ch03/04-end/hotel.js
+++ Ch03/04-end/hotel.js
@@ -1,38 +1,38 @@
-"use strict";
+'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    // This won't work now, since there's more than one
-    // const btnClose = document.querySelector('.btn--dialog-close');
+  // This won't work now, since there's more than one
+  // const btnClose = document.querySelector('.btn--dialog-close');
 
-    // Better to delegate
-    const inHotelSection = document.querySelector('#inhotel');
+  // Better to delegate
+  const inHotelSection = document.querySelector('#inhotel');
 
-    inHotelSection.addEventListener('click', evt => {
-        const target = evt.target;
+  inHotelSection.addEventListener('click', (evt) => {
+    const target = evt.target;
 
-        if (target.classList.contains('btn--dialog-open')) {
-            // This is a dialog opener
-            evt.preventDefault();
+    if (target.classList.contains('btn--dialog-open')) {
+      // This is a dialog opener
+      evt.preventDefault();
 
-            // Get the nearest dialog, should be close
-            let myDialog = target.parentNode.querySelector('.dialog--chef');
-            if (!myDialog) {
-                myDialog = target.parentNode.parentNode.querySelector('.dialog--chef');
-            }
+      // Get the nearest dialog, should be close
+      let myDialog = target.parentNode.querySelector('.dialog--chef');
+      if (!myDialog) {
+        myDialog = target.parentNode.parentNode.querySelector('.dialog--chef');
+      }
 
-            if (myDialog) {
-                myDialog.showModal();
-            }
-        } else if (target.classList.contains('btn--dialog-close')) {
-            // This is a closer
-            evt.preventDefault();
+      if (myDialog) {
+        myDialog.showModal();
+      }
+    } else if (target.classList.contains('btn--dialog-close')) {
+      // This is a closer
+      evt.preventDefault();
 
-            // parentNode would also work, but this is more resilient if the HTML gets deeper
-            const myDialog = evt.target.closest('dialog');
+      // parentNode would also work, but this is more resilient if the HTML gets deeper
+      const myDialog = target.closest('dialog');
 
-            if ('close' in myDialog) {
-                myDialog.close();
-            }
-        }
-    });
+      if ('close' in myDialog) {
+        myDialog.close();
+      }
+    }
+  });
 });
