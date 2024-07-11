5e1329d7e7baa9791c53df8a5fcf0f18028f4c61
chellman <joe@shooflydesign.org>
2024-07-11 17:33:08 +0000

    End state for 05/02

--- Ch05/02-end/components.js
+++ Ch05/02-end/components.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    class StreetAddress extends HTMLElement {
-        static register() {
-            // If our browser can define custom elements
-            if ("customElements" in window) {
-                customElements.define("street-address", StreetAddress);
-            }
-        }
+  class StreetAddress extends HTMLElement {
+    static register() {
+      // If our browser can define custom elements
+      if ('customElements' in window) {
+        customElements.define('street-address', StreetAddress);
+      }
     }
+  }
 
-    StreetAddress.register();
-})
+  StreetAddress.register();
+});
