9701b01a0a1654c768ee15af9bd60bf6b5bb0a2d
chellman <joe@shooflydesign.org>
2024-07-11 21:54:41 +0000

    Formatting pass on 06/4+5

--- Ch06/04-end/game.js
+++ Ch06/04-end/game.js
@@ -1,94 +1,97 @@
-"use strict";
+'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const boardRegion = document.querySelector('.board');
-    const slotsRegion = document.querySelector('.slots');
-    const tilesRegion = document.querySelector('.tiles');
+  const boardRegion = document.querySelector('.board');
+  const slotsRegion = document.querySelector('.slots');
+  const tilesRegion = document.querySelector('.tiles');
 
-    let currentSolution = [];
+  let currentSolution = [];
 
-    function addTileToSolution(tile) {
-        const letter = tile.dataset.letter;
+  function addTileToSolution(tile) {
+    const letter = tile.dataset.letter;
 
-        const targetSlotIndex = currentSolution.length + 1;
-        const targetSlot = document.querySelector(`#slot-${targetSlotIndex}`);
+    const targetSlotIndex = currentSolution.length + 1;
+    const targetSlot = document.querySelector(`#slot-${targetSlotIndex}`);
 
-        // No slots left
-        if (!targetSlot) {
-            return;
-        }
-
-        currentSolution.push(letter);
-        console.log('Solution so far:', currentSolution.join(''));
-
-        // Store the tile's original location if needed
-        if (!tile.dataset.originalLeft) {
-            const originalStyle = getComputedStyle(tile);
-
-            tile.dataset.originalLeft = originalStyle.getPropertyValue('left');
-            tile.dataset.originalTop = originalStyle.getPropertyValue('top');
-            tile.dataset.originalTransform = originalStyle.getPropertyValue('transform');
-        }
-
-
-        tile.style.left = targetSlot.offsetLeft + 'px';
-        tile.style.top = targetSlot.offsetTop + 'px';
-        tile.style.transform = 'none';
+    // No slots left
+    if (!targetSlot) {
+      return;
     }
 
-    function removeTileFromSolution(tile) {
-        const letter = tile.dataset.letter;
+    currentSolution.push(letter);
+    console.log('Solution so far:', currentSolution.join(''));
 
-        // remove the letter from the solution, restore the tile
-        const lastLetter = currentSolution.slice(-1).toString();
+    // Store the tile's original location if needed
+    if (!tile.dataset.originalLeft) {
+      const originalStyle = getComputedStyle(tile);
 
-        if (!lastLetter || letter !== lastLetter) {
-            console.error('Letters must be removed in order:', letter, lastLetter);
-            return;
-        }
-
-        currentSolution = currentSolution.slice(0, -1);
-        console.log('Solution so far:', currentSolution.join(''));
-
-        tile.style.left = tile.dataset.originalLeft;
-        tile.style.top = tile.dataset.originalTop;
-        tile.style.transform = tile.dataset.originalTransform;
+      tile.dataset.originalLeft = originalStyle.getPropertyValue('left');
+      tile.dataset.originalTop = originalStyle.getPropertyValue('top');
+      tile.dataset.originalTransform =
+        originalStyle.getPropertyValue('transform');
     }
 
-    tilesRegion.addEventListener('click', evt => {
-        evt.preventDefault();
+    tile.style.left = targetSlot.offsetLeft + 'px';
+    tile.style.top = targetSlot.offsetTop + 'px';
+    tile.style.transform = 'none';
+  }
 
-        const tile = evt.target;
+  function removeTileFromSolution(tile) {
+    const letter = tile.dataset.letter;
 
-        if (!tile.matches('.tile')) {
-            return;
-        }
+    // remove the letter from the solution, restore the tile
+    const lastLetter = currentSolution.slice(-1).toString();
 
-        if (currentSolution.indexOf(tile.dataset.letter) === -1) {
-            addTileToSolution(tile);
-        } else {
-            removeTileFromSolution(tile);
-        }
-    });
+    if (!lastLetter || letter !== lastLetter) {
+      console.error('Letters must be removed in order:', letter, lastLetter);
+      return;
+    }
 
-    // Listen for presses of the A-F keys and delete
-    document.addEventListener('keyup', evt => {
-        const evtCode = evt.code.toLowerCase();
-        if (currentSolution.length > 0 && (evtCode === 'backspace' || evtCode === 'delete')) {
-            // remove the letter from the solution, restore the tile
-            const lastLetter = currentSolution.slice(-1);
-            const tile = document.querySelector(`.tile[data-letter=${lastLetter}]`);
+    currentSolution = currentSolution.slice(0, -1);
+    console.log('Solution so far:', currentSolution.join(''));
 
-            removeTileFromSolution(tile);
-        } else if (evtCode.match(/key[a-f]/)) {
-            // Thank goodness we don't have to write a separate if block for every letter code!
-            const myLetter = evtCode.match(/key([a-f])/i)[1];
+    tile.style.left = tile.dataset.originalLeft;
+    tile.style.top = tile.dataset.originalTop;
+    tile.style.transform = tile.dataset.originalTransform;
+  }
 
-            if (currentSolution.indexOf(myLetter) === -1) {
-                const tile = document.querySelector(`.tile[data-letter=${myLetter}]`);
+  tilesRegion.addEventListener('click', (evt) => {
+    evt.preventDefault();
 
-                addTileToSolution(tile);
-            }
-        }
-    });
-})
+    const tile = evt.target;
+
+    if (!tile.matches('.tile')) {
+      return;
+    }
+
+    if (currentSolution.indexOf(tile.dataset.letter) === -1) {
+      addTileToSolution(tile);
+    } else {
+      removeTileFromSolution(tile);
+    }
+  });
+
+  // Listen for presses of the A-F keys and delete
+  document.addEventListener('keyup', (evt) => {
+    const evtCode = evt.code.toLowerCase();
+    if (
+      currentSolution.length > 0 &&
+      (evtCode === 'backspace' || evtCode === 'delete')
+    ) {
+      // remove the letter from the solution, restore the tile
+      const lastLetter = currentSolution.slice(-1);
+      const tile = document.querySelector(`.tile[data-letter=${lastLetter}]`);
+
+      removeTileFromSolution(tile);
+    } else if (evtCode.match(/key[a-f]/)) {
+      // Thank goodness we don't have to write a separate if block for every letter code!
+      const myLetter = evtCode.match(/key([a-f])/i)[1];
+
+      if (currentSolution.indexOf(myLetter) === -1) {
+        const tile = document.querySelector(`.tile[data-letter=${myLetter}]`);
+
+        addTileToSolution(tile);
+      }
+    }
+  });
+});
