131af0f1beeffa16efb183405da7ad2d3d0c5e81
chellman <joe@shooflydesign.org>
2024-07-11 18:06:38 +0000

    End 05/03

--- Ch05/03-end/components.js
+++ Ch05/03-end/components.js
@@ -1,31 +1,34 @@
-"use strict";
+'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    class StreetAddress extends HTMLElement {
-        static register() {
-            // If our browser can define custom elements
-            if ("customElements" in window) {
-                customElements.define("street-address", StreetAddress);
-            }
-        }
-
-        formatPostalCode() {
-            const postalCodeLength = 5;
-            const val = this.value || '';
-            let updatedValue = val.replace(/[^0-9]/g, '');
-            if (updatedValue.length > postalCodeLength) {
-                updatedValue = updatedValue.slice(0, postalCodeLength) + '-' + updatedValue.slice(postalCodeLength);
-            }
-
-            this.value = updatedValue;
-        }
-
-        connectedCallback() {
-            const postalInput = this.querySelector('.postal-code input');
-
-            postalInput.addEventListener('input', this.formatPostalCode);
-        }
+  class StreetAddress extends HTMLElement {
+    static register() {
+      // If our browser can define custom elements
+      if ('customElements' in window) {
+        customElements.define('street-address', StreetAddress);
+      }
     }
 
-    StreetAddress.register();
-})
+    formatPostalCode() {
+      const postalCodeLength = 5;
+      const val = this.value;
+      let updatedValue = val.replace(/[^0-9]/g, '');
+      if (updatedValue.length > postalCodeLength) {
+        updatedValue =
+          updatedValue.slice(0, postalCodeLength) +
+          '-' +
+          updatedValue.slice(postalCodeLength);
+      }
+
+      this.value = updatedValue;
+    }
+
+    connectedCallback() {
+      const postalInput = this.querySelector('.postal-code input');
+
+      postalInput.addEventListener('input', this.formatPostalCode);
+    }
+  }
+
+  StreetAddress.register();
+});
