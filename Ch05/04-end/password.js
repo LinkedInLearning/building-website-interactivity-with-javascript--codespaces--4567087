4dec8c71c215b1fedfd476b3430671d13b327041
chellman <joe@shooflydesign.org>
2024-07-11 18:35:35 +0000

    End 05/04

--- Ch05/04-end/password.js
+++ Ch05/04-end/password.js
@@ -1,36 +1,32 @@
-"use strict";
+'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    class PasswordField extends HTMLElement {
-        static register() {
-            // If our browser can define custom elements
-            if ("customElements" in window) {
-                customElements.define("password-field", PasswordField);
-            }
-        }
-
-        connectedCallback() {
-            const btnReveal = this.querySelector('.btn-reveal-password');
-
-            if (btnReveal.innerText !== this.dataset.labelShow) {
-                btnReveal.innerText = this.dataset.labelShow;
-            }
-
-            btnReveal.addEventListener('click', evt => {
-                evt.preventDefault();
-
-                const fldPassword = this.querySelector('.field--password');
-
-                if (fldPassword.type === 'text') {
-                    fldPassword.type = 'password';
-                    btnReveal.innerText = this.dataset.labelShow;
-                } else {
-                    fldPassword.type = 'text';
-                    btnReveal.innerText = this.dataset.labelHide;
-                }
-            });
-        }
+  class PasswordField extends HTMLElement {
+    static register() {
+      // If our browser can define custom elements
+      if ('customElements' in window) {
+        customElements.define('password-field', PasswordField);
+      }
     }
 
-    PasswordField.register();
-})
+    connectedCallback() {
+      const btnReveal = this.querySelector('.btn-reveal-password');
+
+      btnReveal.addEventListener('click', (evt) => {
+        evt.preventDefault();
+
+        const fldPassword = this.querySelector('.field--password');
+
+        if (fldPassword.type === 'text') {
+          fldPassword.type = 'password';
+          btnReveal.innerText = this.dataset.labelShow;
+        } else {
+          fldPassword.type = 'text';
+          btnReveal.innerText = this.dataset.labelHide;
+        }
+      });
+    }
+  }
+
+  PasswordField.register();
+});
