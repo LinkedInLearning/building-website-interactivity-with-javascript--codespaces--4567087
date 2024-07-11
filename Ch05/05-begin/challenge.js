62277d79cbf88ddb0b3bbbbf067e0c64e5e029be
chellman <joe@shooflydesign.org>
2024-07-11 20:48:07 +0000

    Fixing up the Ch5 challenge

--- Ch05/05-begin/challenge.js
+++ Ch05/05-begin/challenge.js
@@ -1,8 +1,11 @@
-"use strict";
+'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    // Step 1: Define the custom element
-
-    // Step 2: Add change event handlers to the quantity and price fields that will update the total field
-    // Hint: they refer to each other, so they can probably use the same event handler
+  // Step 1: Define the custom element
+  //
+  // Step 2: Add change event handlers to the quantity and price fields
+  //         that will update the subtotal field
+  // Hint: they refer to each other, so they can probably use the same event handler
+  //
+  // Step 3: Format the price and subtotal as currency ($0.00)
 });
--- Ch05/06-end/challenge.js
+++ Ch05/06-end/challenge.js
@@ -1,44 +1,45 @@
-"use strict";
+'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    // Step 1: Define the custom element
-    class LineItem extends HTMLElement {
-        static register() {
-            // If our browser can define custom elements
-            if ("customElements" in window) {
-                customElements.define("line-item", LineItem);
-            }
-        }
-
-        connectedCallback() {
-            const fldQty = this.querySelector('.line-item--qty');
-            const fldPrice = this.querySelector('.line-item--price');
-            const fldSubtotal = this.querySelector('.line-item--subtotal');
-
-            // Update this formatted version of the price after we've finished messing around with it.
-            let formattedPrice = fldPrice?.value;
-
-            // Price and quantity changes should both trigger an update of the subtotal
-            const updateSubtotal = evt => {
-                const qty = parseInt(fldQty.value || '0', 10);
-                const priceOnlyNumbers = (fldPrice.value || '0').replace(/[^0-9.]/gi, '');
-                const price = parseFloat(priceOnlyNumbers);
-
-                // Update the subtotal with currency formatting
-                fldSubtotal.value = '$' + (qty * price).toFixed(2);
-
-                // Update the price field with currency as well
-                formattedPrice = '$' + price.toFixed(2);
-            };
-
-            fldQty?.addEventListener('change', updateSubtotal);
-            fldPrice?.addEventListener('input', updateSubtotal);
-
-            fldPrice?.addEventListener('blur', evt => {
-                fldPrice.value = formattedPrice;
-            });
-        }
+  class LineItem extends HTMLElement {
+    static register() {
+      if ('customElements' in window) {
+        customElements.define('line-item', LineItem);
+      }
     }
 
-    LineItem.register();
+    connectedCallback() {
+      const fldQty = this.querySelector('.line-item--qty');
+      const fldPrice = this.querySelector('.line-item--price');
+      const fldSubtotal = this.querySelector('.line-item--subtotal');
+
+      // Update this formatted version of the price after we've finished messing around with it.
+      let formattedPrice = fldPrice.value;
+
+      // Price and quantity changes should both trigger an update of the subtotal
+      const updateSubtotal = () => {
+        const qty = parseInt(fldQty.value || '0', 10);
+        const priceOnlyNumbers = (fldPrice.value || '0').replace(
+          /[^0-9.]/gi,
+          ''
+        );
+        const price = parseFloat(priceOnlyNumbers);
+
+        // Update the subtotal with currency formatting
+        fldSubtotal.value = '$' + (qty * price).toFixed(2);
+
+        // Update the price field with currency as well
+        formattedPrice = '$' + price.toFixed(2);
+      };
+
+      fldQty.addEventListener('change', updateSubtotal);
+      fldPrice.addEventListener('input', updateSubtotal);
+
+      fldPrice.addEventListener('blur', () => {
+        fldPrice.value = formattedPrice;
+      });
+    }
+  }
+
+  LineItem.register();
 });
